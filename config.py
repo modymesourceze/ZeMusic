25564407e098c48ddbba1bc29a51e3941e4b591f
Mody <148470115+modymesourceze@users.noreply.github.com>
2024-02-02 15:21:13 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -77,12 +77,8 @@ autoclean = []
 confirmer = {}
 
 
-START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/41a777f089288f7ad2571.jpg
-)
-PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
-)
+START_IMG_URL = "https://telegra.ph/file/41a777f089288f7ad2571.jpg
+PING_IMG_URL = "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
